b62c28099e110937e136d82c1c2efb07ffdf43ae
Arsh <arshdeepgary@gmail.com>
2020-08-15 20:01:01 -0700

    Updates

---
+++ precache-manifest.d4153f645ba4a52eb36b9f2a1ca092a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a70a67a36f28e950d56e24ddfd5e79",
+    "url": "/conway--gol/index.html"
+  },
+  {
+    "revision": "c0ff769495fd9668644b",
+    "url": "/conway--gol/static/css/main.dcf906d4.chunk.css"
+  },
+  {
+    "revision": "68bb1b3f739132348a3e",
+    "url": "/conway--gol/static/js/2.734de3d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conway--gol/static/js/2.734de3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ff769495fd9668644b",
+    "url": "/conway--gol/static/js/main.4c9c9321.chunk.js"
+  },
+  {
+    "revision": "ada25b02de53ad2a6d33",
+    "url": "/conway--gol/static/js/runtime-main.c89b8d00.js"
+  }
+]);
